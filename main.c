924ecaacecff449c4dd04b822c7957c0fbe0ccb4
Jung Soo Choi <chlwjdtn15@hotmail.com>
2020-06-26 02:49:59 +0000

    for문으로도 해보기

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 void print_str(char* str) {
   int i = 0;
 
-  //배열의 크기는 8바이트 이므로 sizeof를 써서 for문을 돌릴수 없음.(8보다 크면 출력 불가)
-  //배열의 가장 마지막은 항상 null (\0) 이므로 while문을 서서 null이 나올때 까지 돌리면 출력 가능
+  // 배열의 크기는 8바이트 이므로 sizeof를 써서 for문을 돌릴수 없음.(8보다 크면 출력 불가)
+  // 배열의 가장 마지막은 항상 null (\0) 이므로 while문을 서서 null이 나올때 까지 돌리면 출력 가능
 
   while ( 1 ){ //while문으로 일단 돌리기
 
@@ -21,6 +21,20 @@ void print_str(char* str) {
 
 
   }
+
+  //for 문으로 돌리는 방법.
+
+
+  // for (int i = 0; str[i] != '\0'; i++) {
+  //   printf("%c",str[i]);
+
+      //이 printf 말고도 밑에 printf로도 됨.
+      //*(str + 1) == str[i]
+
+    // printf("%c," *(str + i));
+  // }
+
+
 
   printf("\n");
 }
